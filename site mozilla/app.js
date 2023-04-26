69b8f2939e2c735d68f79848afd8e25fc30737e2
nog <andrea.n1@aluno.ifsc.edu.br>
2023-04-26 10:12:24 -0300

    alterações

--- site mozilla/app.js
+++ site mozilla/app.js
@@ -1,12 +1,11 @@
 'use strict'
-FileSystemDirectoryHandle
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
-    if(className = "light-theme")
+    if(className === "light-theme")
     {
         this.textContent = "Dark";
     }
